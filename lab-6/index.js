98cfef5795d77593f04db9c2c6515a1927502bc1
PalesaMonica <2550752@students.wits.ac.za>
2024-04-10 05:38:04 +0200

    new files

--- lab-6/index.js
+++ lab-6/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 const cars = require('./cars.json');
 
