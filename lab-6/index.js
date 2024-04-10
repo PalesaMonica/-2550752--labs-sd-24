f8d0c6701c9d90a5931e6f5e1500fced13fd5f0f
PalesaMonica <2550752@students.wits.ac.za>
2024-04-10 04:20:05 +0200

    modified files

--- lab-6/index.js
+++ lab-6/index.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path'); // Import the path module
 const app = express();
 
-// Use environment variable for port binding
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-
-// Use the CORS middleware
 app.use(cors());
-
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
 
 const cars = require('./cars.json');
 
-// Define API URL environment variable
-const API_URL = process.env.API_URL || 'http://localhost:3001';
+const API_URL = process.env.API_URL || `http://localhost:${PORT}`;
 
-// GET all cars
 app.get('/cars', (req, res) => {
     res.json(cars);
 });
 
-// GET car by id
 app.get('/cars/:id', (req, res) => {
     const id = req.params.id;
     const car = cars.find(car => car.id === id);
     res.json(car);
 });
 
-// PUT update car by id
 app.put('/cars/:id', (req, res) => {
     const id = req.params.id;
     const updatedCar = req.body;
@@ -40,7 +29,6 @@ app.put('/cars/:id', (req, res) => {
     res.json(updatedCar);
 });
 
-// DELETE car by id
 app.delete('/cars/:id', (req, res) => {
     const id = req.params.id;
     const index = cars.findIndex(car => car.id === id);
@@ -48,14 +36,12 @@ app.delete('/cars/:id', (req, res) => {
     res.json({ message: `Car with id ${id} deleted` });
 });
 
-// POST add new car
 app.post('/cars', (req, res) => {
     const newCar = req.body;
     cars.push(newCar);
     res.json(newCar);
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server started at ${API_URL}`);
 });
